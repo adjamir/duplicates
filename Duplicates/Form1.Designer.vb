79a3fa502b8cc88e78bcb3b875a7a067c9faa6fd
Adjamir <franklin.villalobos.v@gmail.com>
2020-01-04 14:34:01 -0500

    ExamineFolder v1

--- Duplicates/Form1.Designer.vb
+++ Duplicates/Form1.Designer.vb
@@ -26,44 +26,96 @@ Partial Class Form1
         Me.btnBrowse = New System.Windows.Forms.Button()
         Me.txtPath = New System.Windows.Forms.TextBox()
         Me.fbdSelectFolder = New System.Windows.Forms.FolderBrowserDialog()
+        Me.FileSystemWatcher1 = New System.IO.FileSystemWatcher()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtFileCount = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtDuplicateCount = New System.Windows.Forms.TextBox()
+        CType(Me.FileSystemWatcher1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'lvFilesSource
         '
         Me.lvFilesSource.HideSelection = False
-        Me.lvFilesSource.Location = New System.Drawing.Point(12, 182)
+        Me.lvFilesSource.Location = New System.Drawing.Point(9, 148)
+        Me.lvFilesSource.Margin = New System.Windows.Forms.Padding(2)
         Me.lvFilesSource.Name = "lvFilesSource"
-        Me.lvFilesSource.Size = New System.Drawing.Size(776, 256)
+        Me.lvFilesSource.Size = New System.Drawing.Size(1065, 314)
         Me.lvFilesSource.TabIndex = 0
         Me.lvFilesSource.UseCompatibleStateImageBehavior = False
         '
         'btnBrowse
         '
-        Me.btnBrowse.Location = New System.Drawing.Point(621, 12)
+        Me.btnBrowse.Location = New System.Drawing.Point(466, 10)
+        Me.btnBrowse.Margin = New System.Windows.Forms.Padding(2)
         Me.btnBrowse.Name = "btnBrowse"
-        Me.btnBrowse.Size = New System.Drawing.Size(156, 32)
+        Me.btnBrowse.Size = New System.Drawing.Size(117, 26)
         Me.btnBrowse.TabIndex = 1
         Me.btnBrowse.Text = "Browse"
         Me.btnBrowse.UseVisualStyleBackColor = True
         '
         'txtPath
         '
-        Me.txtPath.Location = New System.Drawing.Point(12, 17)
+        Me.txtPath.Location = New System.Drawing.Point(9, 14)
+        Me.txtPath.Margin = New System.Windows.Forms.Padding(2)
         Me.txtPath.Name = "txtPath"
-        Me.txtPath.Size = New System.Drawing.Size(592, 22)
+        Me.txtPath.Size = New System.Drawing.Size(445, 20)
         Me.txtPath.TabIndex = 2
         Me.txtPath.Text = "(Click Browse)"
         '
+        'FileSystemWatcher1
+        '
+        Me.FileSystemWatcher1.EnableRaisingEvents = True
+        Me.FileSystemWatcher1.SynchronizingObject = Me
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 52)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(61, 13)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "Files Found"
+        '
+        'txtFileCount
+        '
+        Me.txtFileCount.Location = New System.Drawing.Point(113, 49)
+        Me.txtFileCount.Name = "txtFileCount"
+        Me.txtFileCount.Size = New System.Drawing.Size(100, 20)
+        Me.txtFileCount.TabIndex = 4
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(12, 80)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(90, 13)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Duplicates Found"
+        '
+        'txtDuplicateCount
+        '
+        Me.txtDuplicateCount.Location = New System.Drawing.Point(113, 80)
+        Me.txtDuplicateCount.Name = "txtDuplicateCount"
+        Me.txtDuplicateCount.Size = New System.Drawing.Size(100, 20)
+        Me.txtDuplicateCount.TabIndex = 5
+        '
         'Form1
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.ClientSize = New System.Drawing.Size(1085, 473)
+        Me.Controls.Add(Me.txtDuplicateCount)
+        Me.Controls.Add(Me.txtFileCount)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.txtPath)
         Me.Controls.Add(Me.btnBrowse)
         Me.Controls.Add(Me.lvFilesSource)
+        Me.Margin = New System.Windows.Forms.Padding(2)
         Me.Name = "Form1"
         Me.Text = "Duplicates"
+        CType(Me.FileSystemWatcher1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
@@ -73,4 +125,9 @@ Partial Class Form1
     Friend WithEvents btnBrowse As Button
     Friend WithEvents txtPath As TextBox
     Friend WithEvents fbdSelectFolder As FolderBrowserDialog
+    Friend WithEvents FileSystemWatcher1 As IO.FileSystemWatcher
+    Friend WithEvents Label1 As Label
+    Friend WithEvents txtDuplicateCount As TextBox
+    Friend WithEvents txtFileCount As TextBox
+    Friend WithEvents Label2 As Label
 End Class
--- Duplicates/Form1.vb
+++ Duplicates/Form1.vb
@@ -1,40 +1,75 @@
 ﻿Public Class Form1
+    Public Shared sSelectedPath As String
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles btnBrowse.Click
         If (fbdSelectFolder.ShowDialog() = DialogResult.OK) Then
             txtPath.Text = fbdSelectFolder.SelectedPath
+            sSelectedPath = txtPath.Text
         End If
+        ExamineFolder()
         FillListView()
     End Sub
 
-    Private Sub txtPath_TextChanged(sender As Object, e As EventArgs) Handles txtPath.TextChanged
+    Private Sub ExamineFolder()
+        Dim oFiles = My.Computer.FileSystem.GetFiles(sSelectedPath)
+        Dim sDuplicateList As Array Of String
+        Dim sdupSuffix As String = "(1)"
+        Dim sfilenamecurrentfile As String
+
+        txtFileCount.Text = oFiles.Count
+
+        Dim i As Integer = 0
+        For Each oFile In oFiles
+            i = i + 1
+            sfilenamecurrentfile = oFile
+            sfilenameCFnoExt = Get
+            For Each oFile In oFiles
+                If Trim(sfilenamecurrentfile) & sdupSuffix = oFile Then
+
+                End If
+            Next
+        Next
 
     End Sub
 
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        lvFilesSource.View = View.Details
         lvFilesSource.Columns.Add("Name", 120, HorizontalAlignment.Left)
         lvFilesSource.Columns.Add("Path", 160, HorizontalAlignment.Left)
-        lvFilesSource.Columns.Add("Size", 80, HorizontalAlignment.Left)
-        lvFilesSource.Columns.Add("Created", 120, HorizontalAlignment.Left)
-        lvFilesSource.Columns.Add("Extension", 80, HorizontalAlignment.Left)
+            lvFilesSource.Columns.Add("Size", 80, HorizontalAlignment.Left)
+            lvFilesSource.Columns.Add("Created", 120, HorizontalAlignment.Left)
+            lvFilesSource.Columns.Add("Extension", 80, HorizontalAlignment.Left)
+
     End Sub
     Private Sub FillListView()
-        Using ofd As New OpenFileDialog
-            ofd.Multiselect = True
-            If ofd.ShowDialog = DialogResult.OK Then
-                For Each fn As String In ofd.FileNames
-                    Dim fi As New IO.FileInfo(fn)
-                    Dim lvi As ListViewItem = lvFilesSource.Items.Add(fi.Name)
-                    lvi.SubItems.Add(fi.DirectoryName)
-                    If fi.Length >= 1024 Then
-                        lvi.SubItems.Add((fi.Length / 1024).ToString("F2") & " kb")
-                    Else
-                        lvi.SubItems.Add(fi.Length.ToString & " bytes")
-                    End If
-                    lvi.SubItems.Add(fi.CreationTime.ToString)
-                    lvi.SubItems.Add(fi.Extension)
-                Next
+        Dim oFiles = My.Computer.FileSystem.GetFiles(sSelectedPath)
+        Dim oFile As Object
+
+
+
+        For Each oFile In oFiles
+            Dim oFileInfo As IO.FileInfo = My.Computer.FileSystem.GetFileInfo(oFile)
+            Dim lvi As ListViewItem = lvFilesSource.Items.Add(oFileInfo.Name)
+
+            lvi.SubItems.Add(oFileInfo.DirectoryName)
+            If oFileInfo.Length >= 1024 Then
+                lvi.SubItems.Add((oFile.Length / 1024).ToString() & " kb")
+            Else
+                lvi.SubItems.Add(oFile.Length.ToString & " bytes")
             End If
-        End Using
+            ''lvi.SubItems.Add(fi.CreationTime.ToString)
+            ''lvi.SubItems.Add(fi.Extension)
+        Next
+
+
+    End Sub
+
+    Private Sub lvFilesSource_SelectedIndexChanged(sender As Object, e As EventArgs) Handles lvFilesSource.SelectedIndexChanged
+
+    End Sub
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click, Label2.Click
+
     End Sub
 End Class
 
